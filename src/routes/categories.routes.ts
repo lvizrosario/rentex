169f1da8902b266385d35adbbd2eae15f79b9436
Luiz Carlos <lucafi.araujo@gmail.com>
2021-09-17 08:36:53 -0300

    Services - SRP

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { Category } from '../model/Category'
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -10,8 +11,10 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
+  createCategoryService.execute({ name, description });
+  
   return response.status(201).send();
 });
 
